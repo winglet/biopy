573db5f18f60772391a006414dce6e4e685e5435
vasilis <gkoumplias@gmail.com>
2016-02-04 16:33:57 +0000

    --

--- biopy/challenge1_patterncount.py
+++ biopy/challenge1_patterncount.py
@@ -1,10 +1,8 @@
 
-bioText = "CGTATATGATGAGGACAAGCAGCGTGAGGACACGTATATGAAGCAGCGCGTATATGATGAGGACAAGCAGCGTGAGGACATGAGGACAGTACTGCCGTATATGACGTATATGAGTACTGCTGAGGACAGTACTGCTCGATAATAGTACTGCCGTATATGATGAGGACACGTATATGATGAGGACAGTACTGCGTACTGCCGTATATGATGAGGACAGTACTGCTGAGGACACGTATATGATGAGGACAGTACTGCTCGATAATAAGCAGCGAGCAGCGTGAGGACAGTACTGCTGAGGACAAGCAGCGTGAGGACATGAGGACAAGCAGCGTCGATAATATGAGGACAAGCAGCGCGTATATGACGTATATGACGTATATGAAGCAGCGCGTATATGATGAGGACAGTACTGCGTACTGCTGAGGACAAGCAGCGAGCAGCGTGAGGACAGTACTGCCGTATATGAGTACTGCGTACTGCTGAGGACACGTATATGATCGATAATAGTACTGCAGCAGCGTGAGGACAAGCAGCGCGTATATGACGTATATGAGTACTGCCGTATATGAGTACTGCTCGATAATATCGATAATACGTATATGAAGCAGCGAGCAGCGCGTATATGATGAGGACAAGCAGCGCGTATATGATCGATAATAAGCAGCGAGCAGCGGTACTGCGTACTGCGTACTGCTGAGGACAAGCAGCGCGTATATGACGTATATGATCGATAATAAGCAGCGTCGATAATAAGCAGCGAGCAGCGAGCAGCGAGCAGCGTCGATAATAAGCAGCGTCGATAATAAGCAGCGTCGATAATAGTACTGCCGTATATGAAGCAGCG"
-#bioPattern = "GGGGCACGG"
-k = 11
+bioText = "CGCCTAAATAGCCTCGCGGAGCCTTATGTCATACTCGTCCT"
+bioPattern = "TGT"
 
-
-def patternCount(txt, pat):
+def PatternCount(pat, txt):
     count = 0
     for i in range(0,len(txt)-len(pat)+1):
         #print txt[i:i+len(pat)]
@@ -12,24 +10,54 @@ def patternCount(txt, pat):
             count+=1
     return count
 
-from sets import Set
-def frequentWords(txt, k):
-    kmers = Set()
-    frequency = []
-    max_occurences = 0
-    for i in range(0,len(txt)-k+1):
-        pat = txt[i:i+k]
-        frequency.append( patternCount(txt, pat) )
-        if (frequency[i] >= max_occurences):
-            max_occurences = frequency[i]
-    
-    for i in range(0,len(txt)-k+1):
-        if (frequency[i] == max_occurences):
-            pat = txt[i:i+k]
-            if (pat not in kmers):
-                kmers.add(txt[i:i+k])
-                print str(frequency[i]) + " : " + txt[i:i+k]
-    
 
-frequentWords(bioText, k)
-#print patternCount(bioText, bioPattern)
+def FrequentWords(Text, k):
+    FrequentPatterns = set()
+    Count = CountDict(Text, k)
+    m = max(Count.values())
+    for i in Count:
+        if Count[i] == m:
+            FrequentPatterns.add(Text[i:i+k])
+    return FrequentPatterns
+
+def CountDict(Text, k):
+    Count = {}
+    for i in range(len(Text)-k+1):
+        Pattern = Text[i:i+k]
+        Count[i] = PatternCount(Pattern, Text)
+    return Count
+           
+           
+def reverseComplement(dnatxt):
+    rev = ""
+    c=''
+    strand = dnatxt.upper()
+    for n in strand[::-1]:
+        if  n == 'A': 
+            c = 'T'
+        elif n == 'T':
+            c = 'A'
+        elif n == 'G':
+            c = 'C'
+        elif n == 'C':
+            c = 'G'
+        else :
+            c = '-'
+        rev += str(c)
+    return rev
+
+
+
+
+FrequentWords("GATCCAGATCCCCATAC", 2)
+print PatternCount(bioPattern, bioText)
+print reverseComplement("GCTAGCT")
+exit
+
+import os.path
+with open(os.path.normpath("C:\Users\IBM_ADMIN\Downloads\lol.txt")) as f:
+    lines = f.read().splitlines() 
+    
+print (lines[0])
+print "===================="
+print reverseComplement(lines[0])
